2237a5cb40d8f799051fa2df0c92de1899e46fdd
Steven Ning <stevenning@Stevens-MacBook-Air.local>
2021-08-07 14:28:29 -0600

    Fixed buttons on index page

--- src/components/HomeSlides/HomePageFirst.js
+++ src/components/HomeSlides/HomePageFirst.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './HomePageFirst.css';
-import { BrowserRouter as Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function HomePageFirst()
 {
