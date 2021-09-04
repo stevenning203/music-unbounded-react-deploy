3e6edb4a3d8b4c8775be5b699c7a4419f7239347
stevenning.203 <stevenning.203@gmail.com>
2021-09-04 16:25:01 -0600

    Changed blog date

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -6,7 +6,7 @@ export default function Blog()
 {
     return(
         <div id = "blog-page-wrapper">
-            <BlogEntry blog_link = "/blog/about" content = "This is the Music Unbounded blog! We will post updates here for our organization. Please check reglarly to be aware of recital dates and other events." title = "Nothing here! Check back soon :)" date = "August 5th, 2021" />
+            <BlogEntry blog_link = "/blog/about" content = "This is the Music Unbounded blog! We will post updates here for our organization. Please check reglarly to be aware of recital dates and other events." title = "Nothing here! Check back soon :)" date = "September 8th, 2021" />
         </div>
     )
 }
