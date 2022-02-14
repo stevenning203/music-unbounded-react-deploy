aa03c0ae52b31858b92030438b390ae814e2445a
stevenning.203 <stevenning.203@gmail.com>
2022-02-13 16:02:06 -0800

    Fixed space

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,8 +8,9 @@ function Footer()
         <div id = "__footer">
             <img id = "__footer-logo"src = {Logo}></img>
             <div id = "__footer-blurb">
-                Come check out our social media!: <a href = "https://www.instagram.com/music.unbounded/">Instagram</a> and
-                <a href = "https://www.facebook.com/Music-Unbounded-101698102292596">Facebook</a>
+                Come check out our social media!: <a href = "https://www.instagram.com/music.unbounded/">Instagram</a>
+                and
+                <a href = "https://www.facebook.com/Music-Unbounded-101698102292596"> Facebook</a>
                 <br />
                 <br />
                 Questions? Visit the help and contact pages.
